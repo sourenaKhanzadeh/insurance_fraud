ec5b5979f421a20e8be7cb0f8f7e0f0a996b8d9d
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2020-10-29 20:42:17 -0400

    ...preprocessing 90% finished...

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
 
 
 from abc import ABC, abstractmethod
@@ -21,25 +22,88 @@ class Classifier(ABC):
 
         self.auto_model_vs_fraud_report = self._dataset[['auto_model']]
         self.auto_make_vs_fraud_report = self._dataset[['auto_make']]
+        self.police_availaible_vs_fraud = self._dataset[['police_report_available']]
+        self.property_damage_vs_fraud = self._dataset[['property_damage']]
+        self.incident_city_vs_fraud = self._dataset[['incident_city']]
+        self.incident_state_vs_fraud = self._dataset[['incident_state']]
+        self.authorities_contacted_vs_fraud = self._dataset[['authorities_contacted']]
+        self.incident_severity_vs_fraud = self._dataset[['incident_severity']]
+        self.collision_type_vs_fraud = self._dataset[['collision_type']]
+        self.incident_type_vs_fraud = self._dataset[['incident_type']]
+        self.insured_relationship_vs_fraud = self._dataset[['insured_relationship']]
+        self.insured_hobbies_vs_fraud = self._dataset[['insured_hobbies']]
+        self.insured_occupation_vs_fraud = self._dataset[['insured_occupation']]
+        self.insured_education_level_vs_fraud = self._dataset[['insured_education_level']]
+        self.insured_sex_vs_fraud = self._dataset[['insured_sex']]
+        self.policy_csl_vs_fraud = self._dataset[['policy_csl']]
+        self.policy_state_vs_fraud = self._dataset[['policy_state']]
 
         self.preProcessing()
+
+        # model selection
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self._X, self._y, test_size=0.2, random_state=0)
 
 
     def preProcessing(self):
         #encode the fraud reported column
         label_encoder = LabelEncoder()
         column_transfer =  ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
-
         self._y = label_encoder.fit_transform(self._y)
 
-        # set policy_bind_date to time spam
-        self._X[:, 2] = pd.to_datetime(self._X[:, 2], errors='coerce')
+        # turn dates into time stamp
+        self._X[:, 3] = pd.to_datetime(self._X[:, 3], errors='coerce')
+        self._X[:, 17] = pd.to_datetime(self._X[:, 17], errors='coerce')
 
         # group auto_model with fraud report to find correlation between each
         self.auto_model_vs_fraud_report = self.correlation_encode(self.auto_model_vs_fraud_report, 'auto_model', 36)
 
         # group auto_make with fraud report to find correlation between each
         self.auto_make_vs_fraud_report = self.correlation_encode(self.auto_make_vs_fraud_report, 'auto_make', 35)
+
+        # group police_report_available with fraud report to find correlation between each
+        self.police_availaible_vs_fraud = self.correlation_encode(self.police_availaible_vs_fraud, 'police_report_available', 30)
+
+        # group property_damage with fraud report to find correlation between each
+        self.property_damage_vs_fraud = self.correlation_encode(self.property_damage_vs_fraud, 'property_damage', 27)
+
+        # group incident_city with fraud report to find correlation between each
+        self.incident_city_vs_fraud = self.correlation_encode(self.incident_city_vs_fraud, 'incident_city', 23)
+
+        # group incident_state with fraud report to find correlation between each
+        self.incident_state_vs_fraud = self.correlation_encode(self.incident_state_vs_fraud, 'incident_state', 22)
+
+        # group authorities_contacted with fraud report to find correlation between each
+        self.authorities_contacted_vs_fraud = self.correlation_encode(self.authorities_contacted_vs_fraud, 'authorities_contacted', 21)
+
+        # group incident_severity with fraud report to find correlation between each
+        self.incident_severity_vs_fraud = self.correlation_encode(self.incident_severity_vs_fraud, 'incident_severity', 20)
+
+        # group collision_type with fraud report to find correlation between each
+        self.collision_type_vs_fraud = self.correlation_encode(self.collision_type_vs_fraud, 'collision_type', 19)
+
+        # group incident_type with fraud report to find correlation between each
+        self.incident_type_vs_fraud = self.correlation_encode(self.incident_type_vs_fraud, 'incident_type', 18)
+
+        # group insured_relationship with fraud report to find correlation between each
+        self.insured_relationship_vs_fraud = self.correlation_encode(self.insured_relationship_vs_fraud, 'insured_relationship', 14)
+
+        # group insured_hobbies with fraud report to find correlation between each
+        self.insured_hobbies_vs_fraud = self.correlation_encode(self.insured_hobbies_vs_fraud, 'insured_hobbies', 13)
+
+        # group insured_hobbies with fraud report to find correlation between each
+        self.insured_occupation_vs_fraud = self.correlation_encode(self.insured_occupation_vs_fraud, 'insured_occupation', 12)
+
+        # group insured_education_level with fraud report to find correlation between each
+        self.insured_education_level_vs_fraud = self.correlation_encode(self.insured_education_level_vs_fraud, 'insured_education_level', 11)
+
+        # group insured_sex with fraud report to find correlation between each
+        self.insured_sex_vs_fraud = self.correlation_encode(self.insured_sex_vs_fraud, 'insured_sex', 10)
+
+        # group policy_csl with fraud report to find correlation between each
+        self.policy_csl_vs_fraud = self.correlation_encode(self.policy_csl_vs_fraud, 'policy_csl', 5)
+
+        # group policy_state with fraud report to find correlation between each
+        self.policy_state_vs_fraud = self.correlation_encode(self.policy_state_vs_fraud, 'policy_state', 4)
 
     def correlation_encode(self, model_vs_fraud, model, index):
         model_vs_fraud = model_vs_fraud.join(pd.DataFrame(self._y, columns=['fraud_reported']))
@@ -62,6 +126,12 @@ class Classifier(ABC):
     def describe(self):
         return self._dataset.describe()
 
+    def corr(self):
+        return self._dataset.corr()
+
+    @abstractmethod
+    def fit(self):
+        pass
 
     @property
     def X(self):
@@ -69,6 +139,10 @@ class Classifier(ABC):
     @property
     def y(self):
         return self._y
+
+    @property
+    def dataset(self):
+        return self._dataset
 
     @X.setter
     def X(self, val):
@@ -81,12 +155,36 @@ class Classifier(ABC):
 
 class Logistic(Classifier):
 
-    pass
+    def fit(self):
+        pass
 
 
 if __name__ == "__main__":
     clf = Logistic()
     print(clf.auto_model_vs_fraud_report)
     print(clf.auto_make_vs_fraud_report)
-    print(clf.X[:, 35:37])
+    print(clf.police_availaible_vs_fraud)
+    print(clf.property_damage_vs_fraud)
+    print(clf.incident_city_vs_fraud)
+    print(clf.incident_state_vs_fraud)
+    print(clf.authorities_contacted_vs_fraud)
+    print(clf.incident_severity_vs_fraud)
+    print(clf.collision_type_vs_fraud)
+    print(clf.incident_type_vs_fraud)
+    print(clf.insured_relationship_vs_fraud)
+    print(clf.insured_hobbies_vs_fraud)
+    print(clf.insured_occupation_vs_fraud)
+    print(clf.insured_education_level_vs_fraud)
+    print(clf.insured_sex_vs_fraud)
+    print(clf.policy_csl_vs_fraud)
+    print(clf.policy_state_vs_fraud)
 
+    # print(clf.X[:, 18:24])
+    # print(clf.X[:, 10:15])
+    # print(clf.X[:, 4:6])
+
+    x = pd.DataFrame(clf.x_train, columns=list(clf.dataset.columns)[:-1])
+
+    x = x.drop(['incident_date', 'policy_bind_date', 'incident_location'], axis=1)
+
+    x.astype(float).corr().to_csv("x_train_corr.csv", ",")
