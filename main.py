176f803e968545e700b14c7a2dccb2d822229557
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2020-11-02 12:38:53 -0500

    seaborn installed, heat_map created

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
@@ -15,6 +16,7 @@ class Classifier(ABC):
         self._dataset = pd.read_csv(csv).replace('?', np.NaN)
         self._dataset.isnull().any()
 
+        self.better_disturbute_dataset()
 
         self._X = self._dataset.iloc[:, :-1].values
         self._y = self._dataset.iloc[:, -1].values
@@ -42,7 +44,6 @@ class Classifier(ABC):
 
         # model selection
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self._X, self._y, test_size=0.2, random_state=0)
-
 
     def preProcessing(self):
         #encode the fraud reported column
@@ -105,6 +106,25 @@ class Classifier(ABC):
         # group policy_state with fraud report to find correlation between each
         self.policy_state_vs_fraud = self.correlation_encode(self.policy_state_vs_fraud, 'policy_state', 4)
 
+    def better_disturbute_dataset(self):
+        self._dataset['incident_date'] = pd.to_datetime(self._dataset['incident_date'], errors='coerce')
+
+        # extracting days and month from date
+        self._dataset['incident_month'] = self._dataset['incident_date'].dt.month
+        self._dataset['incident_day'] = self._dataset['incident_date'].dt.day
+        temp = self._dataset
+        self._dataset = self._dataset.drop(['fraud_reported'], axis=1)
+        self._dataset['fraud_reported'] = temp['fraud_reported']
+
+        del temp
+
+        data = self._dataset
+        needed_fraud_columns = int(0.8 * len(data[data['fraud_reported'] == 'Y']))
+        data_fraud = data[data['fraud_reported'] == 'Y'].head(needed_fraud_columns)
+        data_not_fraud = data[data['fraud_reported'] == 'N'].head(needed_fraud_columns)
+
+        self._dataset  = pd.concat([data_fraud, data_not_fraud])
+
     def correlation_encode(self, model_vs_fraud, model, index):
         model_vs_fraud = model_vs_fraud.join(pd.DataFrame(self._y, columns=['fraud_reported']))
         model_vs_fraud = model_vs_fraud.groupby([model],
@@ -118,7 +138,20 @@ class Classifier(ABC):
         return model_vs_fraud
 
 
+    def save_plots(self):
+        pass
 
+    def correlation_heat_map(self):
+        x = pd.DataFrame(self.x_train, columns=list(clf.dataset.columns)[:-1])
+
+        x = x.drop(['incident_date', 'policy_bind_date', 'incident_location'], axis=1)
+
+        plt.figure(figsize=(15, 12))
+        plt.title("correlation matrix")
+        sns.set_context('paper', font_scale=1.4)
+        sns.heatmap(x.astype(float).corr(), annot=False, cmap='Blues')
+        plt.savefig("plots/correlation_matrix.png")
+        plt.clf()
 
     def info(self):
         self._dataset.info()
@@ -160,7 +193,10 @@ class Logistic(Classifier):
 
 
 if __name__ == "__main__":
+    from collections import Counter
     clf = Logistic()
+    count = Counter(clf.y)
+    print(count)
     print(clf.auto_model_vs_fraud_report)
     print(clf.auto_make_vs_fraud_report)
     print(clf.police_availaible_vs_fraud)
@@ -187,4 +223,9 @@ if __name__ == "__main__":
 
     x = x.drop(['incident_date', 'policy_bind_date', 'incident_location'], axis=1)
 
-    x.astype(float).corr().to_csv("x_train_corr.csv", ",")
+    # x.astype(float).corr().to_csv("x_train_corr.csv", ",")
+
+    print(x)
+
+    clf.correlation_heat_map()
+
